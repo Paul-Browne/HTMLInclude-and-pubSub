d0e8829640af5e80d8fbbe9574de27bb8c6836cf
Paul Browne <Paulmatthewbrowne@gmail.com>
2019-07-18 10:42:04 +0300

    Create HTMLInclude-and-PubSub.js

---
+++ HTMLInclude-and-PubSub.js
@@ -0,0 +1,107 @@
+/*! HTMLIncludePS v1.0.0 | MIT License | github.com/paul-browne/HTMLInclude-and-pubSub */ 
+!function(w, d) {
+    if (!w.PS) {
+        w.HTMLInclude = function() {
+            function isInViewport(element, offset) {
+                return element.getBoundingClientRect().top <= (+offset + w.innerHeight);
+            }
+            function ajax(url, elements) {
+                var xhr = new XMLHttpRequest();
+                xhr.onreadystatechange = function() {
+                    if (xhr.readyState == 4 && xhr.status == 200) {
+                        elements.forEach(function(element) {
+                            var dataReplace = element.getAttribute("data-replace");
+                            var z = xhr.responseText;
+                            if (dataReplace) {
+                                dataReplace.split(",").forEach(function(el) {
+                                    var o = el.trim().split(":");
+                                    z = z.replace(new RegExp(o[0], "g"), o[1]);
+                                });
+                            }
+                            element.outerHTML = z;
+                            var scripts = new DOMParser().parseFromString(z, 'text/html').querySelectorAll("SCRIPT");
+                            var i = 0;
+                            var j = scripts.length;
+                            while (i < j) {
+                                var newScript = d.createElement("SCRIPT");
+                                scripts[i].src ? newScript.src = scripts[i].src : newScript.innerHTML = scripts[i].innerHTML;
+                                d.head.appendChild(newScript);
+                                i++;
+                            }
+                        });
+                    }
+                };
+                xhr.open("GET", url, true);
+                xhr.send();
+            }
+            function lazyLoad(element, offset) {
+                w.addEventListener("scroll", function scrollFunc() {
+                    if (isInViewport(element, offset)) {
+                        w.removeEventListener("scroll", scrollFunc);
+                        ajax(element.getAttribute("data-include"), [element]);
+                    }
+                })
+            }
+            var store = {};
+            var dis = d.querySelectorAll('[data-include]:not([data-in])');
+            var i = dis.length;
+            while (i--) {
+                var di = dis[i].getAttribute('data-include');
+                var laziness = dis[i].getAttribute('data-lazy');
+                dis[i].setAttribute("data-in", "");
+                if (!laziness || (laziness && isInViewport(dis[i], laziness))) {
+                    store[di] = store[di] || [];
+                    store[di].push(dis[i]);
+                } else {
+                    lazyLoad(dis[i], laziness);
+                }
+            }
+            for (var key in store) {
+                ajax(key, store[key]);
+            }
+        }
+
+        w.PS = {};
+        var topics = {};
+        var subUid = -1;
+        PS.sub = function(topic, f) {
+            if (!topics[topic]) {
+                topics[topic] = [];
+            }
+            var t = (++subUid)+"";
+            topics[topic].push({
+                t: t,
+                f: f
+            });
+            return t;
+        };
+        PS.pub = function(topic, args) {
+            if (!topics[topic]) {
+                return false;
+            }
+            setTimeout(function() {
+                var subscribers = topics[topic],
+                    len = subscribers ? subscribers.length : 0;
+
+                while (len--) {
+                    subscribers[len].f(topic, args);
+                }
+            }, 0);
+            return true;
+        };
+        PS.unsub = function(t) {
+            for (var m in topics) {
+                if (topics[m]) {
+                    for (var i = 0, j = topics[m].length; i < j; i++) {
+                        if (topics[m][i].t === t) {
+                            topics[m].splice(i, 1);
+                            return t;
+                        }
+                    }
+                }
+            }
+            return false;
+        };
+    }
+
+}(window, document)
